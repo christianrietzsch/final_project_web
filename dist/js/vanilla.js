cdfe3022314cbaad3001d22e8a0322742a8ebdc5
christianrietzsch <rietzsch.christian@gmail.com>
2024-05-02 17:46:57 +0200

    moves shop-javascript to vanilla.js and refactors code to use library

--- dist/js/vanilla.js
+++ dist/js/vanilla.js
@@ -1,3 +1,7 @@
+function $ID (id) {
+  return document.getElementById(id)
+}
+
 async function jsonData(){
   const response = await fetch("daten.json", {cache: "no-store"}); 
   // cache disabled for debugging purposes 
@@ -33,4 +37,43 @@ async function getCurrentCoin() {
 function render(template_obj, dest_obj, data){
   const template = Handlebars.compile(template_obj.innerHTML);
   dest_obj.innerHTML = template(data)
+}
+
+function register_listener(id, function_call) {
+  $ID(id).addEventListener("click", function_call)
+}
+
+function build_json(data) {
+  const str = JSON.stringify(data)
+  const color = (str[str.search('"rate":')+7] == "-" ? "color:red;" : "color:green;")
+  return JSON.parse(str.substring(0, str.length-1) + ',"color": "' + color + '"}')
+}
+
+function send_to_cart(amount, price, name) {
+  localStorage.setItem(name, JSON.stringify({"amount": parseInt(amount), "price": price}))
+}
+
+function init_shop_item(json) {
+  const name = json.name
+  arrow = $ID(name + "-Arrow")
+  arrow.innerHTML = json.rate >= 0 ? "&uarr;" : "&darr;"
+  register_listener(name + "-Buy", 
+    function() {
+      render(modal_template, modal_template.nextElementSibling, json)
+      $ID("buy-btn").addEventListener("click", 
+        function() {
+          let val = amount.value
+          if(val && val > 0) {
+            send_to_cart(val, json.value, json.name)
+            close = $ID("close")
+            close.click()
+          } else {
+            console.log(val)
+          }
+        })
+    })
+  register_listener(name + "-More", 
+    function() {
+      render($ID("more_template"), $ID("more_template").nextElementSibling, json)
+  })
 }
