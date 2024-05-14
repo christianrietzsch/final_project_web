ae1fddccbd87ee66cc7d178ebf3761d11b73549a
christianrietzsch <rietzsch.christian@gmail.com>
2024-05-14 16:25:11 +0200

    updates localStorage to use index

--- dist/js/vanilla.js
+++ dist/js/vanilla.js
@@ -21,10 +21,10 @@ function setCoins(coins){
 }
 
 function removeFromOrdersByID(id) {
-  const orders = getOrders().ord
+  const orders = JSON.parse(localStorage.getItem("orders"))
   let new_orders = []
   for(let i = 0; i < orders.length; i++) {
-    if(orders[i].index != id) {
+    if(orders[i].id != id) {
       new_orders.push(orders[i])
     }
   }
@@ -32,13 +32,12 @@ function removeFromOrdersByID(id) {
 }
 
 function changeOrderAmount(id, amount) {
-  const orders = getOrders().ord
+  const orders = JSON.parse(localStorage.getItem("orders"))
   let new_orders = []
     for(let i = 0; i < orders.length; i++) {
       let new_order = orders[i]
-      if(orders[i].index === id) {
+      if(orders[i].id === id) {
         new_order.amount = amount
-        new_order.end_price = amount*new_order.value
       }
       new_orders.push(new_order)  
     }
@@ -53,10 +52,10 @@ function updateOrder(id, amount) {
   }
 }
 
-function contains_order(orders, index) {
+function contains_order(orders, id) {
   for(let i = 0; i< orders.length; i++) {
-    index = orders[i].index === index
-    if(index) {
+    id = orders[i].id === id
+    if(id) {
       return i;
     }
   }
@@ -66,12 +65,12 @@ function contains_order(orders, index) {
 function addOrder(order) {
   const orders = JSON.parse(localStorage.getItem("orders"))
   if(orders) {
-    index = contains_order(orders, order.index)
-     if(index == -1) {
+    id = contains_order(orders, order.id)
+     if(id == -1) {
      orders.push(order)
      new_orders = orders
    } else {
-     orders[index].amount += order.amount
+     orders[id].amount += order.amount
      new_orders = orders
    }
   } else {
@@ -82,7 +81,14 @@ function addOrder(order) {
 
 function getOrders() {
   let orders = JSON.parse(localStorage.getItem("orders"))
-  return {ord: orders}
+  new_orders = []
+  orders.forEach((order) => {
+    const coin = getCoinByID(order.id)
+    new_order = {...coin, amount: order.amount, 
+      end_price: coin.value*order.amount}
+    new_orders.push(new_order)
+  })
+  return {ord: new_orders}
 }
 
 function getCurrentCoinID(){
@@ -108,67 +114,67 @@ function render(template_obj, dest_obj, data){
   dest_obj.innerHTML = template(data)
 }
 
-document.getElementById('endButton').addEventListener('click', function() {
-  document.getElementById('quizSection').style.display = 'block';
-  loadQuiz();
-});
-
-
-const questions = [
-  {
-    question: "What is the first cryptocurrency?",
-    options: ["Bitcoin", "Ethereum", "Ripple", "Litecoin"],
-    answer: "Bitcoin"
-  },
-  {
-    question: "Who is the creator of Bitcoin?",
-    options: ["Satoshi Nakamoto", "Vitalik Buterin", "Charlie Lee", "Roger Ver"],
-    answer: "Satoshi Nakamoto"
-  },
-  {
-    question: "What technology is used to secure cryptocurrency transactions?",
-    options: ["Blockchain", "Artificial Intelligence", "Cloud Computing", "Big Data"],
-    answer: "Blockchain"
-  }
-];
-
-function loadQuiz() {
-  const quizQuestions = document.getElementById('quizQuestions');
-  quizQuestions.innerHTML = '';
-
-  questions.forEach((question, index) => {
-    const questionElement = document.createElement('div');
-    questionElement.innerHTML = `
-            <h4>Question ${index + 1}: ${question.question}</h4>
-            <div>
-                ${question.options.map((option, i) => `
-                    <div>
-                        <input type="radio" id="q${index}_option${i}" name="q${index}" value="${option}">
-                        <label for="q${index}_option${i}">${option}</label>
-                    </div>
-                `).join('')}
-            </div>
-        `;
-    quizQuestions.appendChild(questionElement);
-  });
-}
-
-document.getElementById('submitQuiz').addEventListener('click', function() {
-  let score = 0;
-  questions.forEach((question, index) => {
-    const selectedOption = document.querySelector(`input[name=q${index}]:checked`);
-    if (selectedOption) {
-      if (selectedOption.value === question.answer) {
-        score++;
-      }
-    }
-  });
-  alert(`Your score: ${score}/${questions.length}`);
-});
-document.getElementById('endButton').addEventListener('click', function() {
-  document.getElementById('startButton').style.display = 'none';
-  document.getElementById('endButton').style.display = 'none';
-  document.getElementById('quizSection').style.display = 'block';
-  loadQuiz();
-});
+//document.getElementById('endButton').addEventListener('click', function() {
+//  document.getElementById('quizSection').style.display = 'block';
+//  loadQuiz();
+//});
+//
+//
+//const questions = [
+//  {
+//    question: "What is the first cryptocurrency?",
+//    options: ["Bitcoin", "Ethereum", "Ripple", "Litecoin"],
+//    answer: "Bitcoin"
+//  },
+//  {
+//    question: "Who is the creator of Bitcoin?",
+//    options: ["Satoshi Nakamoto", "Vitalik Buterin", "Charlie Lee", "Roger Ver"],
+//    answer: "Satoshi Nakamoto"
+//  },
+//  {
+//    question: "What technology is used to secure cryptocurrency transactions?",
+//    options: ["Blockchain", "Artificial Intelligence", "Cloud Computing", "Big Data"],
+//    answer: "Blockchain"
+//  }
+//];
+//
+//function loadQuiz() {
+//  const quizQuestions = document.getElementById('quizQuestions');
+//  quizQuestions.innerHTML = '';
+//
+//  questions.forEach((question, index) => {
+//    const questionElement = document.createElement('div');
+//    questionElement.innerHTML = `
+//            <h4>Question ${index + 1}: ${question.question}</h4>
+//            <div>
+//                ${question.options.map((option, i) => `
+//                    <div>
+//                        <input type="radio" id="q${index}_option${i}" name="q${index}" value="${option}">
+//                        <label for="q${index}_option${i}">${option}</label>
+//                    </div>
+//                `).join('')}
+//            </div>
+//        `;
+//    quizQuestions.appendChild(questionElement);
+//  });
+//}
+//
+//document.getElementById('submitQuiz').addEventListener('click', function() {
+//  let score = 0;
+//  questions.forEach((question, index) => {
+//    const selectedOption = document.querySelector(`input[name=q${index}]:checked`);
+//    if (selectedOption) {
+//      if (selectedOption.value === question.answer) {
+//        score++;
+//      }
+//    }
+//  });
+//  alert(`Your score: ${score}/${questions.length}`);
+//});
+//document.getElementById('endButton').addEventListener('click', function() {
+//  document.getElementById('startButton').style.display = 'none';
+//  document.getElementById('endButton').style.display = 'none';
+//  document.getElementById('quizSection').style.display = 'block';
+//  loadQuiz();
+//});
 
